0c1df5ec151b72ac82c0bb2e57f063b6f0f87fe4
kirbobvlad42-hub <kirbobvlad42@gmail.com>
2025-10-27 14:42:03 +0100

    Add files via upload

--- Form1.cs
+++ Form1.cs
@@ -1,6 +1,7 @@
 using System;
-using Microsoft.Data.Sqlite;
+using System.Data;
 using System.Windows.Forms;
+using Microsoft.Data.Sqlite;
 
 namespace FinalProject
 {
@@ -13,10 +14,31 @@ namespace FinalProject
 
         private void databutton_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("Connected");
+            string connectionString = "Data Source=C:\\SQL\\Trips and Airports1.db";
+
+            try
+            {
+                using (var connection = new SqliteConnection(connectionString))
+                {
+                    connection.Open();
+                    Console.WriteLine("Connected to SQLite database!");
+
+                    string select = "SELECT * FROM Passanger";
+                    using (var command = new SqliteCommand(select, connection))
+                    using (var reader = command.ExecuteReader())
+                    {
+                        DataTable table = new DataTable();
+                        table.Load(reader);
+                        dataGridView1.DataSource = table;
+                        dataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Critical error: " + ex.Message);
+            }
 
         }
-
-                
     }
 }
