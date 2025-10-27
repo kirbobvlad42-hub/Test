ba51289f172c4d75e07c66b74e9514ee1e0070bd
kirbobvlad42-hub <kirbobvlad42@gmail.com>
2025-10-27 13:19:19 +0100

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,59 @@
+﻿namespace FinalProject
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            databutton = new Button();
+            SuspendLayout();
+            // 
+            // databutton
+            // 
+            databutton.Location = new Point(255, 117);
+            databutton.Name = "databutton";
+            databutton.Size = new Size(229, 68);
+            databutton.TabIndex = 0;
+            databutton.Text = "Database";
+            databutton.UseVisualStyleBackColor = true;
+            databutton.Click += databutton_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 332);
+            Controls.Add(databutton);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Button databutton;
+    }
+}
